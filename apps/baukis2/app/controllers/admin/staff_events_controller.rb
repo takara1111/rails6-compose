07bffc48cdd6195303a5cc1d992c34a893fc175f
takara1356 <goblin7283@gmail.com>
2020-04-24 19:56:29 +0900

    [change] staff_eventを返すコードをリファクタリング

--- apps/baukis2/app/controllers/admin/staff_events_controller.rb
+++ apps/baukis2/app/controllers/admin/staff_events_controller.rb
@@ -2,12 +2,11 @@ class Admin::StaffEventsController < Admin::Base
   def index
     if params[:staff_member_id]
       @staff_member = StaffMember.find(params[:staff_member_id])
-      @events = @staff_member
+      @events = @staff_member.events
     else
       @events = StaffEvent
     end
-    @events = @events.order(occrrd_at: :desc)
-    @events = @events.include(:member)
-    @events = @events.page(params[:page])
+    @events =
+      @events.order(occurred_at: :desc).includes(:member).page(params[:page])
   end
 end
